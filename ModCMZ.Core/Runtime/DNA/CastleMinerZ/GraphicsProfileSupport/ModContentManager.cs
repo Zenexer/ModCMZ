3851fbf9a14a153740e949ebfefa6526f6801b54
Zenexer <xyzspfwqiwppp@internal.is>
2021-02-26 02:18:47 -0500

    Custom ContentManager with significant optimizations over the built-in content manager

--- ModCMZ.Core/Runtime/DNA/CastleMinerZ/GraphicsProfileSupport/ModContentManager.cs
+++ ModCMZ.Core/Runtime/DNA/CastleMinerZ/GraphicsProfileSupport/ModContentManager.cs
@@ -3,10 +3,12 @@ using DNA.CastleMinerZ.GraphicsProfileSupport;
 using DNA.Drawing;
 using DNA.Drawing.Imaging;
 using DNA.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -17,243 +19,212 @@ using System.Threading.Tasks;
 
 namespace ModCMZ.Core.Runtime.DNA.CastleMinerZ.GraphicsProfileSupport
 {
-	public class ModContentManager : ContentManager
+	public class ModContentManager : ProfiledContentManager
 	{
-		public static string[] CImageExtensions { get; set; }
+		/// <summary>
+		/// Gets a list of image file patterns for user-overridden textures
+		/// </summary>
+		public static ReadOnlyCollection<string> ImageFilePatterns { get; } = new[]
+		{
+			"*.dds",
+			"*.tiff",
+			"*.tif",
+			"*.png",
+			"*.jpg",
+			"*.jpeg",
+			"*.bmp",
+		}.ToList().AsReadOnly();
 
+		/// <summary>
+		/// Gets or sets the path to the HiDef content folder
+		/// </summary>
 		public string HiDefRoot { get; set; }
+
+		/// <summary>
+		/// Gets or sets the path to the Reach content folder
+		/// </summary>
 		public string ReachRoot { get; set; }
+
+		/// <summary>
+		/// TODO: Unknown purpose
+		/// </summary>
 		public int TextureLevel { get; set; }
-		public bool CheckUserTextures { get; set; }
-		public string UserTexturesDefault { get; set; }
-		public Dictionary<string, string> AvailableTextures { get; set; }
-		public Dictionary<string, Texture2D> PreLoadedTextures { get; set; }
 
-		private Stopwatch m_LoadingTimer;
-		public Stopwatch LoadingTimer
-		{
-			get { return m_LoadingTimer; }
-			set { m_LoadingTimer = value; }
-		}
+		/// <summary>
+		/// Gets or sets a value indicating whether to check for the existence of user-supplied textures before resorting to the default XNB textures
+		/// </summary>
+		public bool IsUserOverridingTextures { get; set; }
 
-		static ModContentManager()
-		{
-			CImageExtensions = new[] { "*.dds", "*.tiff", "*.png", "*.jpg", "*.jpeg", "*.bmp" };
-		}
+		public string ProfileRoot => GraphicsProfileManager.Instance.IsHiDef ? HiDefRoot : ReachRoot;
+
+		/// <summary>
+		/// Gets or sets the path to the user-supplied texture folder
+		/// </summary>
+		public string UserTextureFolder { get; set; }
+		public Dictionary<string, string> AvailableUserTextures { get; set; } = new Dictionary<string, string>();
+		public Dictionary<string, Texture2D> PreloadedUserTextures { get; set; } = new Dictionary<string, Texture2D>();
+
+		private Stopwatch _loadingTimer;
+
+		public string FullRootDirectory => _fullRootDirectory.Value;
+		private Lazy<string> _fullRootDirectory => new Lazy<string>(() =>
+			(string)typeof(ContentManager)
+				.GetField("fullRootDirectory", BindingFlags.Instance | BindingFlags.NonPublic)
+				.GetValue(this)
+		);
+
+		private readonly Dictionary<string, string> ResolvedAssetNames = new Dictionary<string, string>();
 
 		public ModContentManager(IServiceProvider services, string reachRoot, string hiDefRoot, int textureLevel)
 			//: base(services, reachRoot, hiDefRoot, textureLevel)
-			: base(services)
+			: base(services, reachRoot, hiDefRoot, textureLevel)
 		{
+			var argsType = typeof(CastleMinerZGame).Assembly.GetType("DNA.CastleMinerZ.CastleMinerZArgs");
+			var instanceField = argsType.GetField("Instance");
+			var argsInstance = instanceField.GetValue(null);
+			var textureFolderField = argsType.GetField("TextureFolder");
+			var textureFolder = (string)textureFolderField.GetValue(argsInstance) ?? "CustomTextures";
+
 			HiDefRoot = hiDefRoot;
 			ReachRoot = reachRoot;
 			TextureLevel = textureLevel;
-			LoadingTimer = Stopwatch.StartNew();
+			_loadingTimer = Stopwatch.StartNew();
+			UserTextureFolder = textureFolder;
 
-			Type argsType = typeof(CastleMinerZGame).Assembly.GetType("DNA.CastleMinerZ.CastleMinerZArgs");
-			Debug.Assert(argsType != null, "argsType != null");
-			FieldInfo instanceField = argsType.GetField("Instance");
-			Debug.Assert(instanceField != null, "instanceField != null");
-			object argsInstance = instanceField.GetValue(null);
-			Debug.Assert(argsInstance != null, "argsInstance != null");
-			FieldInfo textureFolderField = argsType.GetField("TextureFolder");
-			Debug.Assert(textureFolderField != null, "textureFolderField != null");
-			string textureFolder = (string)textureFolderField.GetValue(argsInstance);
-
-			if (textureFolder != null)
+			if (textureFolder == null)
 			{
-				CheckUserTextures = false;
-				int startIndex = textureFolder.Length + 1;
+				return;
+			}
 
-				foreach (string str in CImageExtensions)
+			//
+			// User is attempting to override textures
+			//
+
+			var textureFolderPrefixLength = textureFolder.Length + 1;
+
+			IsUserOverridingTextures = false;
+			foreach (string searchPattern in ImageFilePatterns)
+			{
+				var files = PathTools.RecursivelyGetFiles(textureFolder, searchPattern);
+				if (files == null || files.Length == 0)
+                {
+					continue;
+                }
+
+				// At least one override
+				IsUserOverridingTextures = true;
+
+				foreach (var file in files)
 				{
-					string[] strArray = PathTools.RecursivelyGetFiles(textureFolder, str);
-					if (strArray != null && strArray.Length > 0)
+					var key = Path.ChangeExtension(file, null).Substring(textureFolderPrefixLength).ToLower();
+
+					if (key.EndsWith("_m"))
 					{
-						if (!CheckUserTextures)
-						{
-							UserTexturesDefault = textureFolder;
-							CheckUserTextures = true;
-							AvailableTextures = new Dictionary<string, string>();
-							PreLoadedTextures = new Dictionary<string, Texture2D>();
-						}
-						foreach (string str2 in strArray)
-						{
-							string key = Path.ChangeExtension(str2, null).Substring(startIndex).ToLower();
-							if (key.EndsWith("_m"))
-							{
-								key = key.Substring(0, key.Length - 2);
-							}
-							if (!AvailableTextures.ContainsKey(key))
-							{
-								AvailableTextures.Add(key, str2);
-							}
-						}
+						key = key.Remove(key.Length - 2);
+					}
+
+					if (!AvailableUserTextures.ContainsKey(key))
+					{
+						AvailableUserTextures.Add(key, file);
 					}
 				}
-			}
-			else
-			{
-				CheckUserTextures = false;
 			}
 		}
 
 		public Texture2D TryLoadFromFile(string assetName)
 		{
-			Texture2D textured;
-
-			if (!PreLoadedTextures.TryGetValue(assetName, out textured))
+			if (PreloadedUserTextures.TryGetValue(assetName, out var textured))
 			{
-				string str2;
-				string key = assetName.ToLower();
-				bool flag = false;
-				flag = AvailableTextures.TryGetValue(key, out str2);
-
-				if (!flag)
-				{
-					if (GraphicsProfileManager.Instance.IsHiDef)
-					{
-						flag = AvailableTextures.TryGetValue(Path.Combine(HiDefRoot.ToLowerInvariant(), key), out str2);
-					}
-					else
-					{
-						flag = AvailableTextures.TryGetValue(Path.Combine(ReachRoot.ToLowerInvariant(), key), out str2);
-					}
-				}
-
-				if (!flag)
-				{
-					return null;
-				}
-
-				bool flag2 = false;
-				key = str2.ToLower();
-				bool makeMipmaps = Path.ChangeExtension(key, null).EndsWith("_m");
-				bool normalizeMipmaps = false;
-
-				if (makeMipmaps)
-				{
-					normalizeMipmaps = key.Contains("_nrm_") || key.Contains("_n_");
-				}
-
-				do
-				{
-					if (GraphicsDeviceLocker.Instance.TryLockDeviceTimed(ref m_LoadingTimer))
-					{
-						flag2 = true;
-						try
-						{
-							textured = TextureLoader.LoadFromFile(CastleMinerZGame.Instance.GraphicsDevice, str2, makeMipmaps, normalizeMipmaps);
-						}
-						finally
-						{
-							GraphicsDeviceLocker.Instance.UnlockDevice();
-						}
-					}
-					if (!flag2)
-					{
-						Thread.Sleep(10);
-					}
-				}
-				while (!flag2);
-
-				PreLoadedTextures.Add(assetName, textured);
+				return textured;
 			}
 
-			return textured;
+            var lowerAssetName = assetName.ToLower();
+
+            if (
+				!AvailableUserTextures.TryGetValue(lowerAssetName, out var userTextureFile) &&
+				!AvailableUserTextures.TryGetValue(Path.Combine(ProfileRoot.ToLowerInvariant(), lowerAssetName), out userTextureFile)
+			)
+            {
+                return null;
+            }
+
+            lowerAssetName = userTextureFile.ToLower();
+            var makeMipmaps = Path.ChangeExtension(lowerAssetName, null).EndsWith("_m");
+            var normalizeMipmaps = false;
+
+            if (makeMipmaps)
+            {
+                normalizeMipmaps = lowerAssetName.Contains("_nrm_") || lowerAssetName.Contains("_n_");
+            }
+
+			// Why is this lock necessary?
+			while (!GraphicsDeviceLocker.Instance.TryLockDeviceTimed(ref _loadingTimer))
+            {
+				Thread.Sleep(10);
+			}
+
+			try
+			{
+				textured = TextureLoader.LoadFromFile(CastleMinerZGame.Instance.GraphicsDevice, userTextureFile, makeMipmaps, normalizeMipmaps);
+			}
+			finally
+			{
+				GraphicsDeviceLocker.Instance.UnlockDevice();
+			}
+
+            PreloadedUserTextures.Add(assetName, textured);
+
+            return textured;
 		}
 
 		public override T Load<T>(string assetName)
 		{
-			Type type = typeof(T);
-			bool flag = type.IsSubclassOf(typeof(Texture)) || (type == typeof(Texture));
-			T local = default(T);
-			LoadingTimer.Restart();
-			if (this.CheckUserTextures && flag)
+			if (ResolvedAssetNames.TryGetValue(assetName, out var knownAssetName))
+            {
+				return base.Load<T>(knownAssetName);
+            }
+
+			var isTexture = typeof(T) == typeof(Texture) || typeof(T).IsSubclassOf(typeof(Texture));
+
+			_loadingTimer.Restart();
+
+			if (IsUserOverridingTextures && isTexture)
 			{
-				object obj = TryLoadFromFile(assetName);
-				//local = Cast<T>(obj);
-				local = obj == null ? default(T) : (T)obj;
-				if (local != null)
+				var texture = TryLoadFromFile(assetName);
+
+				if (texture != null)
 				{
-					return local;
-				}
-			}
-			List<string> list = new List<string>();
-			if ((this.TextureLevel > 1) && flag)
-			{
-				string str = "_L" + this.TextureLevel.ToString();
-				list.Add(assetName + str);
-				if (GraphicsProfileManager.Instance.IsHiDef)
-				{
-					list.Add(this.HiDefRoot + @"\" + assetName + str);
-				}
-				else
-				{
-					list.Add(this.ReachRoot + @"\" + assetName + str);
-				}
-			}
-			list.Add(assetName);
-			if (GraphicsProfileManager.Instance.IsHiDef)
-			{
-				list.Add(this.HiDefRoot + @"\" + assetName);
-			}
-			else
-			{
-				list.Add(this.ReachRoot + @"\" + assetName);
-			}
-
-#if DEBUG
-			List<Exception> exceptions = new List<Exception>(list.Count);
-#endif
-
-			for (int i = 0; i < list.Count; i++)
-			{
-				try
-				{
-					return base.Load<T>(list[i]);
-				}
-				catch (Exception ex)
-				{
-#if DEBUG
-					exceptions.Add(ex);
-#endif
-
-					if (Debugger.IsAttached && File.Exists(Path.Combine(App.Current.TargetDirectory.FullName, RootDirectory, list[i] + ".xnb")))
-					{
-						Type a = Type.GetType("DNA.Drawing.Particles.ParticleEffect, DNA.Common");
-						object b = Activator.CreateInstance(a);
-						bool c = b is T;
-
-						using (Stream stream = OpenStream(list[i]))
-						{
-							using (ContentReader reader = (ContentReader)typeof(ContentReader).GetMethod("Create", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[] { this, stream, list[i], new Action<IDisposable>(d => { }) }))
-							{
-								ContentTypeReader typeReader;
-								var method = typeof(ContentTypeReaderManager).GetMethods(BindingFlags.Static | BindingFlags.NonPublic).First(m => m.Name == "GetTypeReader" && m.GetParameters().Length >= 3);
-								typeReader = (ContentTypeReader)method.Invoke(null, new object[] { "Microsoft.Xna.Framework.Content.ReflectiveReader`1[[DNA.Drawing.Particles.ParticleEffect, DNA.Common]]", reader, new List<ContentTypeReader>(0) });
-								//InstantiateTypeReader("Microsoft.Xna.Framework.Content.ReflectiveReader`1[[DNA.Drawing.Particles.ParticleEffect, DNA.Common]]", reader, out typeReader);
-								b = Activator.CreateInstance(typeReader.TargetType);
-								c = b is T;
-								var d = b.GetType().Assembly.Location;
-								d = typeReader.TargetType.Assembly.Location;
-							}
-						}
-
-
-						throw;
-					}
+					return (T)(object)texture;
 				}
 			}
 
-			throw new Exception("Asset not found " + assetName);
-		}
+			var prefix = ProfileRoot + @"\";
 
-		delegate bool InstantiateTypeReaderD(string readerTypeName, ContentReader contentReader, out ContentTypeReader reader);
-		private static bool InstantiateTypeReader(string readerTypeName, ContentReader contentReader, out ContentTypeReader reader)
-		{
-			var method = (InstantiateTypeReaderD)Delegate.CreateDelegate(typeof(InstantiateTypeReaderD), typeof(ContentTypeReaderManager).GetMethod("InstantiateTypeReader", BindingFlags.Static | BindingFlags.NonPublic));
-			return method(readerTypeName, contentReader, out reader);
+			var resolvedAssetNames = new List<string>();
+
+			if (TextureLevel > 1 && isTexture)
+			{
+				var levelSuffix = "_L" + TextureLevel;
+				resolvedAssetNames.Add(assetName + levelSuffix);
+				resolvedAssetNames.Add(prefix + assetName + levelSuffix);
+			}
+
+			resolvedAssetNames.Add(assetName);
+			resolvedAssetNames.Add(prefix + assetName);
+
+			var resolvedAssets = resolvedAssetNames.Select(x => (resolvedAssetName: x, fileName: Path.Combine(FullRootDirectory, x + ".xnb"))).ToArray();
+
+			foreach (var (resolvedAssetName, fileName) in resolvedAssets)
+			{
+				if (File.Exists(fileName))
+                {
+					var value = base.Load<T>(resolvedAssetName);
+					ResolvedAssetNames.Add(assetName, resolvedAssetName);  // Ensure this runs after it loads so we don't store something that throws an exception
+					return value;
+				}
+			}
+
+			throw new FileNotFoundException($"Content not found: {assetName} -> {string.Join(", ", resolvedAssets.Select(x => x.fileName))}");
 		}
 	}
 }
